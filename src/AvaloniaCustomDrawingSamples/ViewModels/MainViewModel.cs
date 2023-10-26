1970c2bb7b0c5df26495a764bdb11d8895edd6e6
Magnus Lindhe <magnus.lindhe@emway.se>
2023-10-26 13:36:10 +0200

    Initial solution

---
+++ src/AvaloniaCustomDrawingSamples/ViewModels/MainViewModel.cs
@@ -0,0 +1,9 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace AvaloniaCustomDrawingSamples.ViewModels
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
---
+++ src/AvaloniaCustomDrawingSamples/Views/MainView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace AvaloniaCustomDrawingSamples.Views
+{
+    public partial class MainView : UserControl
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+    }
+}
